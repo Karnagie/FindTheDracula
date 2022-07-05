96704c3e68bc4c3ad11227ec897db97415f4c40a
Karnagie <vova333dec@gmail.com>
2022-07-05 15:18:35 +0300

    Ragdoll and cleaning test scene

---
+++ Assets/Scripts/Core/AnimationEssence/RootRepeater.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Core.AnimationEssence
+{
+    public class RootRepeater : MonoBehaviour
+    {
+        [SerializeField] private Transform _mainRoot;
+        [SerializeField] private List<TransformPair> _pairs;
+
+        private List<Transform> _mainChilds = new List<Transform>();
+        private List<Transform> _childs = new List<Transform>();
+
+        private void Update()
+        {
+            foreach (var pair in _pairs)
+            {
+                pair.Repeat();
+            }
+        }
+
+
+        private void OnValidate()
+        {
+            if(_mainRoot == null) return;
+            
+            AddChildren(transform);
+            AddMainChildren(_mainRoot);
+
+            for (int i = 0; i < _mainChilds.Count; i++)
+            {
+                _pairs.Add(new TransformPair(_mainChilds[i], _childs[i]));
+            }
+        }
+
+        private void AddChildren(Transform parent)
+        {
+            foreach (Transform child in parent)
+            {
+                _childs.Add(child);   
+                AddChildren(child);
+            }
+        }
+        
+        private void AddMainChildren(Transform parent)
+        {
+            foreach (Transform child in parent)
+            {
+                _mainChilds.Add(child);   
+                AddMainChildren(child);
+            }
+        }
+    }
+    
+    [Serializable]
+    public class TransformPair
+    {
+        public Transform Main;
+        public Transform Child;
+
+        public TransformPair(Transform main, Transform child)
+        {
+            Main = main;
+            Child = child;
+        }
+
+        public void Repeat()
+        {
+            Child.transform.position = Main.transform.position;
+            Child.transform.rotation = Main.transform.rotation;
+        }
+    }
+}
